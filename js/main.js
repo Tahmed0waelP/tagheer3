b967688453c76726dafb70a59ae0ddf2d05c5c35
Tahmed0waelP <medo71640@gmail.com>
2022-07-31 21:31:33 +0200

    text

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ window.setInterval(() => {
   homeBgNum = Math.floor(Math.random() * homeBgs.length);
   $('.home').css('background', 'url(\'' + homeBgs[homeBgNum] + '\')');
 }, 1000);
+
+
+// Yasreb
+// 4*KO2 + 2*CO2 = 2*K2CO3 + 3*O2
+// Variables
+currentCarbonRationInAir = prompt(); // The current carbon vakue in the air that will get from the sensor
+normalCardonRationInAir = 50; // The Normal Carbon Ration On The Air
+unnormalCarbonRatioInAir = currentCarbonRationInAir - normalCardonRationInAir; // The carbon ration that more than the normal carbon ration on the air
+ko2Ratio = unnormalCarbonRatioInAir * 2; // The KO2 ratio
+console.log('KO2 Ration is: ' + ko2Ratio);
