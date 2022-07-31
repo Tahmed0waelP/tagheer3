f01f9a667fb303e7ebfbc1492db31629037979bd
Tahmed0waelP <medo71640@gmail.com>
2022-07-31 16:02:46 +0200

    text

--- js/main.js
+++ js/main.js
@@ -5,5 +5,5 @@ $('.toggle-icon').click(() => {
 window.setInterval(() => {
   homeBgs = ['../images/home/bg_1.png', '../images/home/bg_2.png', '../images/home/bg_3.png'];
   homeBgNum = Math.floor(Math.random() * homeBgs.length);
-  $('.home').css('background', 'url(' + $[homeBgs[homeBgNum]] + ')';
+  $('.home').css('background', 'url(\'' + homeBgs[homeBgNum] + '\')');
 }, 1000);
